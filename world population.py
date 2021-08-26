df6b558ce3b78fa6fdcddd71124422e91fd66305
Haziq Ali <haziqaliofficial@gmail.com>
2021-08-27 00:54:34 +0500

    commit on main

---
+++ world population.py
@@ -0,0 +1,12 @@
+import json
+# Load the data into a list.
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+# Print the 2010 population for each country.
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2000':
+        country_name = pop_dict['Country Name']
+        population = int(float(pop_dict['Value']))
+        Year=pop_dict['Year']
+        print(country_name + ": " + str(population)+" in " +Year)
