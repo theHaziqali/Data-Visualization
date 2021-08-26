25a025f6345e38a6b0c5a0a584e8a7441abe532d
Haziq Ali <haziqaliofficial@gmail.com>
2021-08-26 23:11:11 +0500

    commit on main

--- highs_low.py
+++ highs_low.py
@@ -1,23 +1,36 @@
 import csv
 from matplotlib import pyplot as plt
-# Get high temperatures from file.
-filename = 'sitka_weather_07-2014.csv'
+from datetime import datetime
+# Get dates and high temperatures from file.
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
-        high = int(row[1])
-        highs.append(high)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+        
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
 #print(highs)
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
-
+plt.plot(dates, highs, c='red', alpha=.5)
+plt.plot(dates, lows, c='blue', alpha=.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=.1)
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures of death valley - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
+fig.autofmt_xdate()
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
 """     print(header_row)
